9e176e8125b0a07b3ccd190eb4b05dc30b5f7023
Ignacio Donoso <naxonicolas1997@gmail.com>
2025-01-05 16:53:16 -0300

    Docker compose added, readme added

--- 3. Acceso a datos en entornos Node/3. Acceso a datos relacionales y no relacionales con ORM y ODM en Node y Express/src/routes/sale.routes.ts
+++ 3. Acceso a datos en entornos Node/3. Acceso a datos relacionales y no relacionales con ORM y ODM en Node y Express/src/routes/sale.routes.ts
@@ -5,12 +5,12 @@ import private_routes from "../middlewares/private_routes";
 
 const sale_routes: Router = Router();
 
-sale_routes.post("/", private_routes, (_req: Request, res: Response) => {
-    createSale(_req, res);
+sale_routes.post("/", private_routes, async (_req: Request, res: Response) => {
+    await createSale(_req, res);
 });
 
-sale_routes.get("/:id", private_routes, (_req: Request, res: Response) => {
-    findSaleById(_req, res);
+sale_routes.get("/:id", private_routes, async (_req: Request, res: Response) => {
+    await findSaleById(_req, res);
 });
 
 export default sale_routes;
